e9fc6c1d3b93576b25eb670c3e5f6f46b66c8266
Karo Hambaryan <harohambaryan@gmail.com>
2024-03-13 22:22:07 +0400

    added Layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AppRoutes from './AppRoutes.tsx'
+import "./global.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
