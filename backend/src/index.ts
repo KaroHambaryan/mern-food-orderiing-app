1812299baae3830f961a140d283f78e2090d9095
Karo Hambaryan <harohambaryan@gmail.com>
2024-03-13 18:10:42 +0400

    added mongo DB

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
 import express, { Request, Response } from 'express'
-import cors from 'cors';
-import 'dotenv/config';
+import cors from 'cors'
+import 'dotenv/config'
+import mongoose from 'mongoose'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello!" })
+mongoose
+.connect(process.env.MONGODB_CONNECTION_STRING as string)
+.then(() => {
+	console.log('connected yo database!')
 })
 
-app.listen(7000, ()=>{
-  console.log("server started on localhost: 7000");
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/test', async (req: Request, res: Response) => {
+	res.json({ message: 'Hello!' })
+})
+
+app.listen(7000, () => {
+	console.log('server started on localhost: 7000')
 })
