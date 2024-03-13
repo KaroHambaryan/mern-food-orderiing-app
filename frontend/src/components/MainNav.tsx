46343ca2efcdab1d31d55584d5245e66c19a58c5
Karo Hambaryan <harohambaryan@gmail.com>
2024-03-14 01:17:08 +0400

    MainNav component

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+   return (
+      <Button 
+        variant='ghost'
+        className="front-bold hover:text-orange-500 hover:bg-white"
+      >
+        Log In
+      </Button>
+   )
+}
+
+export default MainNav;
